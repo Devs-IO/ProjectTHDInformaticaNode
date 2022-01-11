de19695840cfeb6f0e5647175c205e09dc25a108
WendellOliver <wendellofmendes@gmail.com>
2022-01-11 18:06:56 -0300

    feature/create-client

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express from 'express';
+import 'reflect-metadata';
 
 const app = express();
 app.use(express.json());
 
 app.listen(3333, () => {
-  console.log('🎃Server started on port 3333🎊')
+  console.log('🎃Server started on port 3333🎊');
 });
