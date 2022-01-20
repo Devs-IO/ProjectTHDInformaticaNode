8a29294e19511912c04994d68230ef45ec1e7c64
WendellOliver <wendellofmendes@gmail.com>
2022-01-20 18:48:36 -0300

    feat: delete consolelog

--- src/middlewares/checksClientValidate.ts
+++ src/middlewares/checksClientValidate.ts
@@ -6,16 +6,14 @@ const checksUserValidate =
     async (request: Request, response: Response, next: NextFunction) => {
 
       const { name, phone, email, cpf, city } = request.body;
-      console.log(request.body);
+
       try {
         await requestSchema.validate({
           name, phone, email, cpf, city
         });
-        console.log("cheguei aqui");
 
         return next();
       } catch (error) {
-        console.log("cheguei aqui antes do erro", error);
         throw new Error();
       }
     }
