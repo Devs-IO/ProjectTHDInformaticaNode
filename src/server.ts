a78549b676b2066c0330cef82524e19c3cf9dcfc
rebecanonato89 <rebecanonato89@gmail.com>
2022-02-08 12:34:04 -0300

    feat: cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import cors from 'cors';
 import express from "express";
 import routes from "./routes/routes"
 import "./database/index";
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
