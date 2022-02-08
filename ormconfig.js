e3022484e6ca632dd7e6daf5880b58412bed0100
rebecanonato89 <rebecanonato89@gmail.com>
2022-02-08 11:41:15 -0300

    feta ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,8 @@ console.log('process.env.DATABASE_URL :>> ', process.env.DATABASE_URL);
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: true,
+  extra: { ssl: { rejectUnauthorized: false } },
   entities: ['dist/models/**/*.js'],
   migrations: ['dist/database/migrations/**/*.js'],
   cli: {
