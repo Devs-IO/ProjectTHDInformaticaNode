ad7e4af6a1102f56433f61a3e40e50f0b8cfc71e
rebecanonato89 <rebecanonato89@gmail.com>
2022-02-11 13:40:27 -0300

    feat: orm

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'default',
   type: 'postgres',
   url: process.env.DATABASE_URL,
   ssl: true,
   extra: { ssl: { rejectUnauthorized: false } },
-  entities: ['./src/models/*.ts', 'dist/models/*.js'],
+  //entities: ['./src/models/*.ts'],
+  entities: ['dist/models/**/*.js'],
   migrations: ['dist/database/migrations/**/*.js'],
   cli: {
     migrationsDir: ['src/database/migrations/'],
