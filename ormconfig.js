8c7df559c0d9618df340df5da492cabb188f3d57
rebecanonato89 <rebecanonato89@gmail.com>
2022-02-11 13:38:21 -0300

    feat: orm

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,8 @@ module.exports = {
   url: process.env.DATABASE_URL,
   ssl: true,
   extra: { ssl: { rejectUnauthorized: false } },
-  entities: ['./src/models/*.ts', 'dist/models/**/*.js'],
+  //entities: ['./src/models/*.ts'],
+  entities: ['dist/models/**/*.js'],
   migrations: ['dist/database/migrations/**/*.js'],
   cli: {
     migrationsDir: ['src/database/migrations/'],
